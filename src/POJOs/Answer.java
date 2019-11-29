ade27478de24a88898190d9fa58123f9b7899229
juanbalsads <juan.balsa.ds@gmail.com>
2019-11-29 14:05:37 +0100

    hh

--- src/POJOs/Answer.java
+++ src/POJOs/Answer.java
@@ -11,14 +11,15 @@ import java.io.Serializable;
  *
  * @author juanb
  */
-public class Answer implements Serializable{
+public class Answer implements Serializable {
 
     public static final String STOP = "stop";
     public static final String VALID = "valid";
     public static final String VALID_USERNAME = "validusername";
     public static final String ERR = "error";
     public static final String NEWUN = "qwerty";
-
+    public static final String CLOSE = "close";
+    public static final String REPEAT = "repeat";
     private String answer = null;
 
     public Answer(String answer) {
